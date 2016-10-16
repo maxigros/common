bffd86665174198a0ca157ee4def4fb81da099be
anton <anton>
2016-10-16 15:13:53 +0300

    shablon for stm32l152

---
+++ shab/flash.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK Utility\ST-LINK_CLI.exe" -c SWD -ME -P "Objects/project.hex" 0x08000000 -Rst -Run
+
---
+++ shab/main.c
@@ -0,0 +1,11 @@
+#include <stm32l1xx.h>
+void delay(int msec)
+{
+	for (;msec > 0; msec--);
+}
+void Init()
+{
+}
+int main()
+{
+}
